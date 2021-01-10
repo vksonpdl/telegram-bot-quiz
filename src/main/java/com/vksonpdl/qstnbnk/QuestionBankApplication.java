e9cfa9040de8c1e48f64a414957fb666489785bf
Vikas Patoli <vksonpdl@gmail.com>
2021-01-10 20:07:17 +0530

    Initial Check-in

---
+++ src/main/java/com/vksonpdl/qstnbnk/QuestionBankApplication.java
@@ -0,0 +1,13 @@
+package com.vksonpdl.qstnbnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionBankApplication.class, args);
+	}
+
+}
